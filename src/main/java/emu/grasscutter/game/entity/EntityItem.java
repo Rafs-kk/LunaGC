47a118a89aba589c86f52ddf9f3cf7406e258fda
Rafs-kk <83425447+Rafs-kk@users.noreply.github.com>
2026-06-04 11:18:22 -0300

    Fix dropped item pickup by restoring TrifleGadget payload
    Import TrifleGadget and update gadgetInfo builder

--- src/main/java/emu/grasscutter/game/entity/EntityItem.java
+++ src/main/java/emu/grasscutter/game/entity/EntityItem.java
@@ -22,6 +22,7 @@ import emu.grasscutter.net.proto.SceneGadgetInfoOuterClass.SceneGadgetInfo;
 import emu.grasscutter.net.proto.VectorOuterClass.Vector;
 import emu.grasscutter.server.packet.send.PacketGadgetInteractRsp;
 import emu.grasscutter.utils.helpers.ProtoHelper;
+import emu.grasscutter.net.proto.TrifleGadgetOuterClass.TrifleGadget;
 import it.unimi.dsi.fastutil.ints.Int2FloatMap;
 import lombok.Getter;
 
@@ -144,7 +145,7 @@ public class EntityItem extends EntityBaseGadget {
         SceneGadgetInfo.Builder gadgetInfo =
                 SceneGadgetInfo.newBuilder()
                         .setGadgetId(this.getItemData().getGadgetId())
-                        // .setTrifleGadget(TrifleGadget.newBuilder().setItem(this.getItem().toProto()))
+                        .setTrifleGadget(TrifleGadget.newBuilder().setItem(this.getItem().toProto()))
                         .setBornType(GadgetBornType.GADGET_BORN_TYPE_IN_AIR)
                         .setAuthorityPeerId(this.getWorld().getHostPeerId())
                         .setIsEnableInteract(true);
